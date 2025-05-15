1bad1e2cc55c4778c4426c38441b29faac4604fa
kaydwithers <kaydwithers@gmail.com>
2025-05-15 19:51:28 +1000

    feat: add components

---
+++ src/app/components/OurStory/OurStory.tsx
@@ -0,0 +1,72 @@
+export const OurStory = () => {
+  return (
+    <div className="mb-32">
+      <h2>Our story</h2>
+
+      <div className="">
+        <p>
+          Eva and Kayd would like to thank each of you for being here today.
+          It’s an honour to have you here. As two introverts, they chose to keep
+          their wedding small and personal. If you’re here, it’s because you
+          hold a very special place in their lives.
+        </p>
+        <p>
+          Eva and Kayd didn’t meet at church (as they sometimes like to joke),
+          but online, in November 2022. Their first date took place in the city,
+          squeezed into a tight window of time before Eva flew to Indonesia to
+          visit her mother, who was unwell. Eva lived in Geelong and Kayd in
+          South Melbourne. When Kayd saw Eva he couldn’t believe his luck.
+        </p>
+        <p>
+          Kayd was nervous and knew of a couple of “cool” places to go for
+          dinner. All the cool places Kayd knew were full and the two wandered
+          the city together. Eva had never walked so much and was mindful of her
+          departing SkyBus. Eva pointed to a modest Chinese restaurant and said,
+          “Let’s just go here.” It may not have looked special from the outside,
+          but it became just that—they spent the evening talking and laughing,
+          mostly about dogs.
+        </p>
+        <p>
+          Not long after, in July 2022, Kayd welcomed a Shih Tzu puppy into his
+          life—one that his cousin Flynn named Cookie. Eva had been campaigning
+          for a Shih Tzu for some time, and while Kayd was originally a pug
+          enthusiast, he grew to like them. Cookie became an important part of
+          their lives. Kayd and Eva used to go to the movies a lot and out for
+          dinners. But then Cookie came along and they preferred quiet nights at
+          home hanging out with her. They had a new family member and started to
+          understand what it might be like to have a child. Sadly Cookie could
+          not be in the room with us today but she would have liked a pat from
+          each of you.
+        </p>
+        <p>
+          Eventually, Kayd moved to Geelong to be closer to Eva. A little while
+          later Chloe arrived. Eva and Kayd did think they were a bit too old
+          for kids but it turned out that wasn’t true. Eva put Kayd under a lot
+          of pressure to get married. Kayd didn’t like the idea of marriage,
+          mostly because he did not want to throw a big wedding party and be in
+          front of 100’s of people that his parents would have invited.
+          Eventually Kayd realised that Eva is very special to him and decided
+          to propose.
+        </p>
+        <p>
+          Kayd proposed to Eva on their favourite Anglesea beach on January 28th
+          2024. They have since moved back to Melbourne to be closer to loved
+          ones. In November, Eva’s sister Elisa moved in with them. Now a
+          household of four in a cozy two-bedroom apartment, they’ve created a
+          home full of love, support, and the occasional chaos. They are so
+          grateful for all the help that Elisa has provided. Kayd’s parents
+          Adele and Trevor also help them out a lot and they are so grateful for
+          all the help.
+        </p>
+        <p>
+          Eva and Kayd are deeply grateful for the people who have walked
+          alongside them on this journey—and even more grateful to share today
+          with you.
+        </p>
+
+        <p>With love,</p>
+        <p>Eva & Kayd</p>
+      </div>
+    </div>
+  );
+};
---
+++ src/app/components/Seating/Seating.tsx
@@ -0,0 +1,67 @@
+export const Seating = () => {
+  return (
+    <div className="flex flex-col gap-y-48">
+      <h2>Seating</h2>
+
+      <div className="">
+        <div className="grid grid-cols-10 text-center">
+          <p>1. Shiny</p>
+          <p>2. Joan</p>
+          <p>3. Adele</p>
+          <p>4. Flynn</p>
+          <p>5. Kayd</p>
+          <p>6. Eva</p>
+          <p>7. Chloe</p>
+          <p>8. Johan</p>
+          <p>9. Ian</p>
+          <p>10. Andrew</p>
+        </div>
+
+        <div className="grid grid-cols-10 border h-48"></div>
+
+        <div className="grid grid-cols-10 text-center">
+          <p>11. Matt</p>
+          <p>12. Merv</p>
+          <p>13. Trevor</p>
+          <p>14. Aston</p>
+          <p>15. Antony</p>
+          <p>16. Haeli</p>
+          <p>17. Elisa</p>
+          <p>18. Rena</p>
+          <p>19. Novi</p>
+          <p>20. Eka</p>
+        </div>
+      </div>
+
+      <div className="">
+        <div className="grid grid-cols-10 text-center">
+          <p>21. Mark</p>
+          <p>22. Michael</p>
+          <p>23. Andrew Dobrich</p>
+          <p>24. Chris</p>
+          <p>25. Thomas</p>
+          <p>26. Andrew Davidson</p>
+          <p>27. Amel</p>
+          <p>28. Rutika</p>
+          <p>29. Suppawat</p>
+          <p>30. Aldea</p>
+        </div>
+
+        <div className="grid grid-cols-10 border h-48"></div>
+
+        <div className="grid grid-cols-10 text-center">
+          <p>31. Trish</p>
+          <p>32. Ayesha</p>
+          <p>33. Jess</p>
+          <p>34. Tweety</p>
+          <p>35. Sunny</p>
+          <p>36. Tony</p>
+          <p>37. Sayeed</p>
+          <p>38. Gapy</p>
+          <p>39. Esther</p>
+          <p>40. Vania</p>
+        </div>
+      </div>
+    </div>
+  );
+};
